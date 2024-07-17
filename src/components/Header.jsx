9b19cca36341690909c81ac1b44ca2c004de8c3c
Mahan <mahanwebwins@gmail.com>
2024-07-17 14:29:31 +0330

    context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,20 @@ import React, { Component, useContext } from "react";
 import { DashboardContext } from "../context/context";
 
 const Header = () => {
-  const {userLoggedIn , setUserLoggedIn} = useContext(DashboardContext)
-  let authUser = userLoggedIn.isAuth
+  const { userLoggedIn, setUserLoggedIn } = useContext(DashboardContext);
+  let authUser = userLoggedIn.isAuth;
   return (
     <div className="header mb-6 px-6 d-flex jc-sb">
       <div>Dashboard</div>
-      {authUser ? <div>Name: {userLoggedIn.name}</div> : <div>Please login!</div>}
-      <button onClick={() => setUserLoggedIn({...userLoggedIn , isAuth : !authUser})}>
-        {authUser ? 'Log out' : 'Login'}
+      {authUser ? (
+        <div>Name: {userLoggedIn.name}</div>
+      ) : (
+        <div>Please login!</div>
+      )}
+      <button
+        onClick={() => setUserLoggedIn({ ...userLoggedIn, isAuth: !authUser })}
+      >
+        {authUser ? "Log out" : "Login"}
       </button>
     </div>
   );
